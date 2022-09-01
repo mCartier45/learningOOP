3e0d68d9e6005bcae017eee0e84bace55cd288a7
mCartier45 <mcartier445@gmail.com>
2022-09-01 16:45:52 -0400

    comit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class Item:
     discount = 0.8  # Pay rate after 20% discount
+    all = []
 
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to received arguments
@@ -11,11 +12,17 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
     def apply_discount(self):
         self.price *= self.discount
+
+    def __repr__(self):
+        return f"Item('{self.name}, {self.price}, {self.quantity}')"
 
 item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
@@ -23,6 +30,11 @@ item3 = Item("Cable", 10, 5)
 item4 = Item("Mouse", 50, 5)
 item5 = Item("Keyboard", 75, 5)
 
+print(Item.all)
+""""
+for item in Item.all:
+    print(item.name + " " + (str)(item.price) + " " + (str)(item.quantity))
+"""
 
 
 
